2e180d17f23ff6486ee5b07b8a6e9763dc91bb81
direyesm <37608963+direyesm@users.noreply.github.com>
2020-09-07 21:29:03 -0300

    LOG.D

--- app/src/main/java/com/example/roomexample/FirstFragment.kt
+++ app/src/main/java/com/example/roomexample/FirstFragment.kt
@@ -1,13 +1,16 @@
 package com.example.roomexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.roomexample.database.Task
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -16,12 +19,31 @@ class FirstFragment : Fragment() {
 
     lateinit var viewModel: TaskViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        viewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+
+
+        //Insertando algo a la lista
+        val task = Task(1, "Algo de prueba", false)
+        val task01 = Task(2, "Algo de prueba 1", false)
+        val task02 = Task(3, "Algo de prueba 2", false)
+        viewModel.insertTask(task)
+        viewModel.insertTask(task01)
+        viewModel.insertTask(task02)
+
+
+        //Escuchar liveData del ViewModel
+        viewModel.allTask.observe(viewLifecycleOwner, Observer {
+            Log.d("OBJETO", it.toString())
+        })
 
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
